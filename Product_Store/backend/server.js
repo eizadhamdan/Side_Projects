119f1264692fcbfa02293a87daedbfdeb615ff3b
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-12-25 21:21:47 +0530

    add delete method for products

--- Product_Store/backend/server.js
+++ Product_Store/backend/server.js
@@ -32,7 +32,19 @@ app.post("/api/products", async (req, res) => {
     }
 });
 
-console.log(process.env.MONGO_URI);
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+    // console.log("id: ", id);
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted successfully." });
+    } catch (error) {
+        res.status(404).json({ success: false, message: "Product not found." });
+    }
+});
+
+// console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
     connectDB();
