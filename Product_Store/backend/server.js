29aa7ae87913f367d9c5c3f517a8ea5147c6d7ba
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-12-25 21:00:37 +0530

    connect api

--- Product_Store/backend/server.js
+++ Product_Store/backend/server.js
@@ -2,16 +2,35 @@
 import express from 'express';
 import dotenv from "dotenv";
 import {connectDB} from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());            // this is a middleware that allows us to accept json data in the req.body
+
 // app.get("/products", (req, res) => {
 //    res.send("Server is ready!");
 // });
 
-app.post("/products", (req, res) => {});
+app.post("/api/products", async (req, res) => {
+    const product = req.body;       // user will send this data
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all the fields." });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.error("Error in creating product: ", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
 
 console.log(process.env.MONGO_URI);
 
