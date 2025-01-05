6abe447c24c2d7dd2685489852bef1bc9bf092b5
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2025-01-05 23:27:35 +0530

    update server

--- Product_Store/backend/server.js
+++ Product_Store/backend/server.js
@@ -1,9 +1,9 @@
-// const express = require('express');
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import {connectDB} from "./config/db.js";
-// import Product from "./models/product.model.js";
-// import mongoose from "mongoose";
+import path from "path";
+
+import { connectDB } from "./config/db.js";
+
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
@@ -11,17 +11,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());            // this is a middleware that allows us to accept json data in the req.body
+const __dirname = path.resolve();
 
-// app.get("/products", (req, res) => {
-//    res.send("Server is ready!");
-// });
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-// console.log(process.env.MONGO_URI);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log("Server started at http://localhost:"+PORT);
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
 });
